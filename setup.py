85d31c1cf2da78072e5a72ffca845025fadb04c9
Yurii Shutko <yshutko@example.com>
2021-01-06 16:16:00 +0000

    Created project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+setuptools.setup(
+    name="HR",
+    version="0.1.0",
+    author="Yurii",
+    author_email="yurii@example.com",
+    description="Python package to manage users on a server based on an “inventory” JSON file",
+    long_description=long_description,
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+)
