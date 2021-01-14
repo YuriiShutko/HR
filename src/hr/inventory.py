3e467496a0d2812a9dc0f314a7f96052b944dbe6
Yurii Shutko <yshutko@example.com>
2021-01-14 20:32:44 +0000

    Implemented inventory dump function

--- src/hr/inventory.py
+++ src/hr/inventory.py
@@ -1,5 +1,24 @@
 import json
+from helpers import get_user_names
+import spwd
+import grp
 
-def inventory_read(filename):
-    with open(filename, 'r') as f:
+def read(path):
+    with open(path, 'r') as f:
         return json.load(f)
+
+def dump(path, user_names=get_user_names()):
+    users = []
+    for user_name in user_names:
+        password = spwd.getspnam(user_name).sp_pwd
+        groups = _groups_for_user(user_name)
+        users.append({
+            'name': user_name,
+            'groups': groups,
+            'password': password
+        })
+    with open(path, 'w') as f:
+        json.dump(users, f)
+
+def _groups_for_user(user_name):
+    return [g.gr_name for g in grp.getgrall() if user_name in g.gr_mem]
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -29,7 +29,7 @@ def test_inventory_read():
             ]
     ''')
     test_inventory.close()
-    users_list = inventory.inventory_read(test_inventory.name)
+    users_list = inventory.read(test_inventory.name)
     assert users_list[0] == {
         "name": "kevin",
         "groups": ["wheel", "dev"],
@@ -45,3 +45,26 @@ def test_inventory_read():
         "groups": [],
         "password": "pass3"
     }
+
+def test_inventory_dump(mocker):
+    """
+    inventory_dump function should take a destination path and and optional list of users to export then exports the existing user information
+    """
+    destination_file = tempfile.NamedTemporaryFile(delete=False)
+    destination_file.close()
+
+    # spwd.getspnam can't be used by non-root user normally. To test it use Mock
+    mocker.patch('spwd.getspnam', return_value=mocker.Mock(sp_pwd='password'))
+
+    # grp.getgrall will return the values from the test machine.
+    # To get consistent results we need to mock this.
+    mocker.patch('grp.getgrall', return_value=[
+        mocker.Mock(gr_name='super', gr_mem=['bob']),
+        mocker.Mock(gr_name='other', gr_mem=[]),
+        mocker.Mock(gr_name='wheel', gr_mem=['bob', 'kevin']),
+    ])
+
+    inventory.dump(destination_file.name, ['kevin', 'bob'])
+
+    with open(destination_file.name) as f:
+        assert f.read() == """[{"name": "kevin", "groups": ["wheel"], "password": "password"}, {"name": "bob", "groups": ["super", "wheel"], "password": "password"}]"""
