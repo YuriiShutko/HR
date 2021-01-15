aef53ca3830a4fdf38fd3bc9c6946f2c5ad84591
Yurii Shutko <yshutko@example.com>
2021-01-15 11:01:07 +0000

    Added function that generates a crypted pass to helpers module

--- src/hr/helpers.py
+++ src/hr/helpers.py
@@ -1,4 +1,13 @@
 import pwd
+from getpass import getpass
+from crypt import crypt, METHOD_SHA512
 
 def get_user_names():
     return [user.pw_name for user in pwd.getpwall() if user.pw_uid >= 1000 and 'home' in user.pw_dir]
+
+def gen_crypt_password():
+    password = getpass()
+    if password == getpass('Please repeat: '):
+        print('\n'+crypt(password, METHOD_SHA512))
+    else:
+        print('\nFailed repeating.')
